d0b2fd9aab55c91b45bbf916d28c7df7e89ef36b
Adriel-oa <107728966+Adriel-oa@users.noreply.github.com>
2025-03-21 20:12:37 +0000

    fix eslint linting

--- pages/api/v1/migrations/index.js
+++ pages/api/v1/migrations/index.js
@@ -1,7 +1,6 @@
 import migrationRunner from "node-pg-migrate";
 import { join } from "node:path";
 import database from "infra/database.js";
-import { request } from "node:http";
 
 export default async function migrations(request, response) {
   const allowMethods = ["GET", "POST"];
