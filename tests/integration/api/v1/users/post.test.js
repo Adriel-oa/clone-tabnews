29c1749808de0675f1e3d888aec774e9807c8987
Adriel-oa <107728966+Adriel-oa@users.noreply.github.com>
2025-04-02 22:32:35 +0000

    feat: add migration to create `users` table

---
+++ tests/integration/api/v1/users/post.test.js
@@ -0,0 +1,27 @@
+import orchestrator from "tests/orchestrator.js";
+import database from "infra/database.js";
+import { use } from "react";
+
+beforeAll(async () => {
+  await orchestrator.waitForAllServices();
+  await orchestrator.clearDatabase();
+  await orchestrator.runPendingMigrations();
+});
+
+describe("POST /api/v1/migrations", () => {
+  describe("Anonymous user", () => {
+    test("With unique and valid data", async () => {
+      await database.query({
+        text: "INSERT INTO users (username, email, password) VALUES ($1, $2, $3);",
+        values: ["adrieloda", "aoda@mail.com", "senha123"],
+      });
+      const users = await database.query("SELECT * FROM USERS;");
+      console.log(users.rows);
+
+      const response = await fetch("http://localhost:3000/api/v1/users", {
+        method: "POST",
+      });
+      expect(response.status).toBe(201);
+    });
+  });
+});
